d51c549ce8bcae02c766badf15f8ab97b8454744
kkingsbe <kkingsbe@gmail.com>
2018-06-28 11:14:08 -0400

    First Commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,15 +1,22 @@
 package sample;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
 public class Controller {
     MongoClientURI uri = new MongoClientURI(
-            "mongodb+srv://kay:BCCBarons2016!@cluster0.mongodb.net/");
+            "mongodb+srv://<kkingsbe>:<BCCBarons2016!>@moneyapp-qcubp.mongodb.net/test?retryWrites=true");
 
     MongoClient mongoClient = new MongoClient(uri);
+
     MongoDatabase database = mongoClient.getDatabase("moneyApp");
-
-
+    MongoCollection<Document> Messages = database.getCollection("Messages");
+    BasicDBObject document = new BasicDBObject();
+    document.put("name", "Shubham");
+    document.put("company", "Baeldung");
+    Messages.insert(document);
 }
